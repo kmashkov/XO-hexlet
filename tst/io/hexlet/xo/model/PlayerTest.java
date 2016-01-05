61dfaf95079bad2a585b520d68888c30003c218c
KonstantinMashkov <mashkov.85@gmail.com>
2016-01-05 21:15:47 +0300

    PlayerTest created

---
+++ tst/io/hexlet/xo/model/PlayerTest.java
@@ -0,0 +1,26 @@
+package io.hexlet.xo.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PlayerTest {
+    
+    @Test
+    public void testGetName() throws Exception {
+        final String inputValue = "Kostya";
+        final String expectedValue = inputValue;
+        final Player player = new Player(inputValue, null);
+        final String actualValue = player.getName();
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void testGetFigure() throws Exception {
+        final Figure inputValue = Figure.X;
+        final Figure expectedValue = inputValue;
+        final Player player = new Player(null, inputValue);
+        final Figure actualValue = player.getFigure();
+        assertEquals(expectedValue, actualValue);
+    }
+}
