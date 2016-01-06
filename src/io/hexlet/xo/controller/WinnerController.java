dc3bc6baaac69dfff3cdc50c58667d09ca34cd83
KonstantinMashkov <mashkov.85@gmail.com>
2016-01-06 14:56:26 +0300

    WinnerController created

---
+++ src/io/hexlet/xo/controller/WinnerController.java
@@ -0,0 +1,59 @@
+package io.hexlet.xo.controller;
+
+import io.hexlet.xo.model.Field;
+import io.hexlet.xo.model.Figure;
+import io.hexlet.xo.model.exceptions.InvalidPointException;
+
+import java.awt.*;
+
+public class WinnerController {
+
+    public Figure getWinner(Field field) {
+        try {
+            for (int i = 0; i < field.height; i++) {
+                if (check(field, new Point(i, 0), p -> new Point(p.x, p.y + 1))) {
+                    return field.getFigure(new Point(i, 0));
+                }
+            }
+            for (int i = 0; i < field.width; i++) {
+                if (check(field, new Point(0, i), p -> new Point(p.x + 1, p.y))) {
+                    return field.getFigure(new Point(0, i));
+                }
+            }
+            if (check(field, new Point(0, 0), p -> new Point(p.x + 1, p.y + 1))) {
+                return field.getFigure(new Point(field.width, field.height));
+            }
+            if (check(field, new Point(field.width, field.height), p -> new Point(p.x - 1, p.y - 1))) {
+                return field.getFigure(new Point(field.width, field.height));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private boolean check(Field field, Point currentPoint, PointGenerator pointGenerator) {
+        final Point nextPoint;
+        final Figure currentFigure;
+        final Figure nextFigure;
+        try {
+            currentFigure = field.getFigure(currentPoint);
+            nextPoint = pointGenerator.next(currentPoint);
+            nextFigure = field.getFigure(nextPoint);
+        } catch (InvalidPointException e) {
+            return true;
+        }
+        if (currentFigure == null) {
+            return false;
+        }
+        if (currentFigure != nextFigure) {
+            return false;
+        }
+        return check(field, nextPoint, pointGenerator);
+    }
+
+    private interface PointGenerator {
+        Point next(final Point point);
+    }
+
+}
